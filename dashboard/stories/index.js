c5b8873034a5ec3f6a9baa81e3aaffba18d9b36c
Baudry_de-BRAY <baudrydebray@gmail.com>
2019-04-06 00:13:40 +0200

    storybook maj

--- dashboard/stories/index.js
+++ dashboard/stories/index.js
@@ -3,6 +3,13 @@ import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 import Widgets from '../Widgets/Widgets'; //modifier chemin?
 
+import Area from '../Widgets/Area';
+import Bar from '../Widgets/Bar';
+import Line from '../Widgets/Line';
+import Pie from '../Widgets/Pie';
+import Radar from '../Widgets/Radar';
+import Scatter from '../Widgets/Scatter';
+
 storiesOf('Button', module)
   .add('with text', () => (
     <Button>Hello Button</Button>
@@ -10,4 +17,11 @@ storiesOf('Button', module)
   .add('with emoji', () => (
     <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
   ));  
-  storiesOf('Widgets', module).add("Widgets", () => <Widgets />);
+
+storiesOf('Widgets', module)
+  .add("Area widget", () => <Area />)
+  .add("Bar widget", () => <Bar />)
+  .add("Line widget", () => <Line />)
+  .add("Pie widget", () => <Pie />)
+  .add("Radar widget", () => <Radar />)
+  .add("Scatter widget", () => <Scatter />);
